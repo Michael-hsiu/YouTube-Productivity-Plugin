cdd3e7542f771fa9c7c367e4584d4e7c6bdab1c8
Michael Hsiu <michael.hsiu@berkeley.edu>
2017-01-29 21:47:39 -0800

    Dialog text working, overlay working, close tab working. Cheers~

--- YT Test Demo/background.js
+++ YT Test Demo/background.js
@@ -1,8 +1,43 @@
 // Called when the user clicks on the browser action.
 chrome.browserAction.onClicked.addListener(function(tab) {
-  // No tabs or host permissions needed!
-  console.log('Turning ' + tab.url + ' red!');
-  chrome.tabs.executeScript({
-    code: 'document.body.style.backgroundColor="red"'
-  });
+	// No tabs or host permissions needed!
+	console.log('Turning ' + tab.url + ' red!');
+	chrome.tabs.executeScript({
+		code: 'document.body.style.backgroundColor="red"'
+	});
+
+
+
 });
+
+chrome.runtime.onMessage.addListener(function(response, sender, sendResponse) {
+	// response holds content.js message
+	// sender holds info about info about tab sending info to this script
+	// sendResponse sends response back to content.js, if needed
+
+	if (response == "closeTab") {
+		// Get the current active tab
+    	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+			var current = tabs[0]
+			// Close current active tab
+			chrome.tabs.remove(current.id);
+		});
+	}
+});
+
+
+
+
+
+/*
+	var activeTabId;
+
+	chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+		activeTabId = tabs[0];
+	});
+	chrome.tabs.remove(activeTabId);
+
+  	chrome.tabs.getSelected(null, function(tab) {
+    	chrome.tabs.remove(tab.id);
+  	});
+*/
--- YT Test Demo/content.js
+++ YT Test Demo/content.js
@@ -33,69 +33,104 @@ function afterNavigate() {
 		if (videoCount >= 0) {
 			//alert("You have watched " + videoCount + " videos!");
 
-			// Create overlay dialog
+			// Create background black layer
 			wrapperDiv = document.createElement("div");
-			//wrapperDiv.setAttribute("style","position: relative; background-color: rgb(0,0,0); opacity: 0.5; z-index: 2000; height: 100%; width: 100%;");
-			wrapperDiv.setAttribute("style","position: absolute; left: 0px; top: 0px; background-color: rgb(0, 0, 0); opacity: 0.5; z-index: 2000; height: 10000px; width: 10000px;");
+			wrapperDiv.setAttribute("style","position: absolute; left: 0px; top: 0px; background-color: rgb(0, 0, 0); opacity: 0.95; z-index: 2000; height: 10000px; width: 10000px;");
 
-			iframeElement = document.createElement("iframe");
-			iframeElement.setAttribute("style","width: 100%; height: 100%;");
+			// CSS test
+			//wrapperDiv.className = "wrapperDiv";	// Possibly try w/ background script?
 
-			wrapperDiv.appendChild(iframeElement);
+			// Create iFrame (not needed)
+			//iframeElement = document.createElement("iframe");
+			//iframeElement.setAttribute("style","width: 100%; height: 100%;");
+
+			//wrapperDiv.appendChild(iframeElement);
 
 			modalDialogParentDiv = document.createElement("div");
-			modalDialogParentDiv.setAttribute("style","position: absolute; width: 800px; border: 1px solid rgb(51, 102, 153); padding: 10px; background-color: rgb(255, 255, 255); z-index: 2001; overflow: auto; text-align: center; top: 149px; left: 200px");
+			modalDialogParentDiv.setAttribute("style","position: absolute; width: 800px; border: 1px solid rgb(51, 102, 153); padding: 10px; background-color: rgb(255, 255, 255); z-index: 2001; overflow: auto; text-align: left; top: 80px; left: 100px");
+			modalDialogParentDiv.style.position = "fixed";
+
 
 			modalDialogSiblingDiv = document.createElement("div");
+			//modalDialogSiblingDiv.style.position = "fixed";
+
 
 			modalDialogTextDiv = document.createElement("div"); 
 			modalDialogTextDiv.setAttribute("style" , "text-align:center");
+			//modalDialogTextDiv.style.position = "fixed";
+
 
 			modalDialogTextSpan = document.createElement("span"); 
 			modalDialogText = document.createElement("strong"); 
-			modalDialogText.innerHTML = "You've been watching too many videos!";
+			modalDialogText.innerHTML = "You've been watching too many videos! Your laziness ain't helping nobody. Get your shit together.";
+
+			modalDialogText.style.fontSize = "80px";
+			modalDialogText.style.fontStyle = "italic";
+			modalDialogText.style.fontWeight = "900";
+			modalDialogText.style.position = "fixed";
+			modalDialogText.style.color = "#ff0000";
+
 
 			imageElement = document.createElement("img"); 
 			imageElement.src = chrome.extension.getURL("stop.jpg");
+			//imageElement.style.position = "fixed";
 
 			breakElement = document.createElement("br"); 
 		    btn = document.createElement("BUTTON");
-    		t = document.createTextNode("CLICK ME");
+    		t = document.createTextNode("Click me");
     		btn.appendChild(t);
 
 
 			modalDialogTextSpan.appendChild(modalDialogText);
 			modalDialogTextDiv.appendChild(modalDialogTextSpan);
-			modalDialogTextDiv.appendChild(breakElement);
-			modalDialogTextDiv.appendChild(breakElement);
+			//modalDialogTextDiv.appendChild(breakElement);
+			//modalDialogTextDiv.appendChild(breakElement);
 
-			modalDialogTextDiv.appendChild(imageElement);
+			//modalDialogTextDiv.appendChild(imageElement);
 
 			modalDialogSiblingDiv.appendChild(modalDialogTextDiv);
     		//modalDialogParentDiv.appendChild(btn)
 			modalDialogParentDiv.appendChild(modalDialogSiblingDiv);
 
-			document.body.appendChild(wrapperDiv);
+			document.body.appendChild(wrapperDiv);		// Background layer
 			document.body.appendChild(modalDialogParentDiv);
+
+			document.body.appendChild(btn);
+
+			/*// Get current tabID
+			var activeTabId;
+			//doInCurrentTab( function(tab){ activeTabId = tab.id } );
+
+			chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+  				activeTabId = tabs[0];
+			});
+
+			chrome.tabs.insertCSS(activeTabId, {
+				file: "dom.css"
+			}); */
 
 			// When the user clicks anywhere outside of the modal, close it
 			window.onclick = function(event) {
-    		if (event.target == imageElement) {
-    			wrapperDiv.style.display = "none";
-        		modalDialogParentDiv.style.display = "none";
-    }
-}
+	    		if (event.target == modalDialogText) {
+	    			//modalDialogText.style.color = "magenta";
+	    			wrapperDiv.style.display = "none";
+	        		modalDialogParentDiv.style.display = "none";
+	        		chrome.runtime.sendMessage("closeTab");		// Send message to background script
+	        		
+				}
+			}
 		}
     }
 }
 
 
-
+// YouTube detection logic
 (document.body || document.documentElement).addEventListener('transitionend',
   function(/*TransitionEvent*/ event) {
     if (event.propertyName === 'width' && event.target.id === 'progress') {
         afterNavigate();
     }
 }, true);
+
 // After page load
 afterNavigate();
