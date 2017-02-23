a251ab087c596d77ccdaecba94262e34e0f27fac
Michael Hsiu <michael.hsiu@berkeley.edu>
2017-02-22 19:56:26 -0800

    More code re-organization, headers added, more cleanup. We're getting close to the end...

--- YouTube Blocker v1/content.js
+++ YouTube Blocker v1/content.js
@@ -5,30 +5,14 @@
  *  goals, hours watched, and video watch history based on keywords (to come later).
  */
 
-var videoCount = 0;
-var lastMsgIndex;
-var videoRatio;		// How many videos watched before notification
 
-// Potential messages
-profaneNotifications = [];
-profaneNotifications.push("You've been watching too many videos! Your laziness ain't helping nobody. Get your shit together.");
-profaneNotifications.push("Fuck your bullshit. Lazy shitter. Get off your ass.");
-profaneNotifications.push("You're lazy. Do you want to be?");
-profaneNotifications.push("Ain't nobody gonna hold your hand in this tough world. Start busting ass and taking names now.");
-profaneNotifications.push("Life ain't all sunshines and YouTube. Take the pledge to better yourself.");
-profaneNotifications.push("Thought is nothing. Execution is everything. Close this fucking tab.");
-profaneNotifications.push("Get moving. Whether that's in the mind or body, just get moving.");
-
-cleanNotifications = [];
-cleanNotifications.push("You've been watching too many videos! Your laziness ain't helping nobody. Get it together.");
-cleanNotifications.push("This ain't the way to go. Get off your bum.");
-cleanNotifications.push("You're lazy. Do you want to be?");
-cleanNotifications.push("Ain't nobody gonna hold your hand in this tough world. Start working hard now before it's too late.");
-cleanNotifications.push("Life ain't all sunshines and YouTube. Take the pledge to better yourself.");
-cleanNotifications.push("Thought is nothing. Execution is everything. Close the tab.");
-cleanNotifications.push("Get moving. Whether that's in the mind or body, just get moving.");
+// Instance variables
+var videoCount = 0;     // Total video count (all time)
+var lastMsgIndex;       // Index of last displayed message (not working)
+var videoRatio;		    // Message is displayed every this number of videos
 
 
+/** CHROME STORAGE ACCESS */
 
 // Gets the difficulty and creates video counter.
 chrome.storage.sync.get('difficulty', function (data) {
@@ -64,7 +48,7 @@ chrome.storage.sync.get({
 
 
 // Asynchronous calls can be tricky! (chrome.storage.sync especially)
-function afterNavigate() {
+function navigatedToNewPage() {
 	/** Sometimes registers as 1 or 2 videos watched.
 	 * Registers as 2 videos if re-navigating from YT home page.
 	 * Registers as 0 videos if manually navigating from suggested at the end of video.
@@ -98,209 +82,221 @@ function afterNavigate() {
 
 			// Alert every 'videoRatio' videos watched
 			//if (videoCount % videoRatio == 0) {
-			if (videoCount >= 0) {
-				//alert("VIDEO RATIO: " + videoRatio);
-				//alert("You have watched " + videoCount + " videos!");
+            if (videoCount < 0) {
+            } else {
+                //alert("VIDEO RATIO: " + videoRatio);
+                //alert("You have watched " + videoCount + " videos!");
 
-				/************************************** CREATING THE UI **************************************/
+                /************************************** CREATING THE UI **************************************/
 
-				// Programmatically creates the UI that occurs every time videoRatio is reached.
+                // Programmatically creates the UI that occurs every time videoRatio is reached.
 
-				/** BACKGROUND */
+                /** BACKGROUND */
 
-				// Create background white layer (partially opaque)
-				var backgroundDiv = document.createElement("div");
-				backgroundDiv.setAttribute("style",
-					"position: fixed; " +
-					"width: 800px; " +
-					"height: 1500px; " +
-					"left: 0px; " +
-					"top: 0px; " +
-					"background-color: #ffffff; " +
-					"opacity: 0.98; " +
-					"z-index: 2100000000; " +
-					"width: 100%;");
+                    // Create background white layer (partially opaque)
+                var backgroundDiv = document.createElement("div");
+                backgroundDiv.setAttribute("style",
+                    "position: fixed; " +
+                    "width: 800px; " +
+                    "height: 1500px; " +
+                    "left: 0px; " +
+                    "top: 0px; " +
+                    "background-color: #ffffff; " +
+                    "opacity: 0.98; " +
+                    "z-index: 2100000000; " +
+                    "width: 100%;");
 
-				/** MESSAGE + BUTTON */
-				// Create the parent div
-				var messageParentDiv = document.createElement("div");
+                /** MESSAGE + BUTTON */
+                    // Create the parent div
+                var messageParentDiv = document.createElement("div");
 
-				// Make div follow scrolling and set relative z-index on page
-				messageParentDiv.setAttribute("style",
-					"position: fixed; " +
-					"opacity: 1; " +
-					"z-index: 2100000001; " +
-					"overflow: auto; " +
-                    "left: 5%; " +
-					"top: 200px; " +
-					"position: fixed; " +
-					"alignSelf: center");
+                // Make div follow scrolling and set relative z-index on page
+                messageParentDiv.setAttribute("style",
+                    "position: fixed; " +
+                    "opacity: 1; " +
+                    "z-index: 2100000001; " +
+                    "overflow: auto; " +
+                    "margin: auto;" +
+                    "position: fixed; " +
+                    "left: -15px; ");
 
-				// Create a div to hold div containing messageText
-				var messageDivContainer = document.createElement("div");
+                messageParentDiv.style.alignSelf = "center";
 
-				// Create div for span containing messageText
-				var messageDiv = document.createElement("div");
-				messageDiv.setAttribute("style",
-					"text-align: center; " +
-					"padding: 35px; " +
-					"alignSelf: parent");
+                // Create a div to hold div containing messageText
+                var messageDivContainer = document.createElement("div");
 
-				// Create text span for messageText
-				var messageTextSpan = document.createElement("span");
-                messageTextSpan.setAttribute("style",
-                    "alignSelf: parent");
+                // Create div for span containing messageText
+                var messageDiv = document.createElement("div");
+                messageDiv.setAttribute("style",
+                    "text-align: center; " +
+                    "padding: 35px; ");
+                messageDiv.style.alignSelf = "center";
 
-				// Create the messageText (initially empty)
-				var messageText = document.createElement("strong");
+                // Create text span for messageText
+                var messageTextSpan = document.createElement("span");
+                messageTextSpan.style.alignSelf = "center";
+                messageTextSpan.style.textOverflow = "ellipsis";
+
+                // Create the messageText (initially empty)
+                var messageText = document.createElement("strong");
 
 
+                /**************************** GETTING A MESSAGE TO DISPLAY ****************************/
 
-				/*********************************** GETTING A MESSAGE TO DISPLAY ***********************************/
+                // Assign random message (more of just in case they find a way to bypass extension and don't close tab)
+                /** RANDOM MESSAGE GENERATOR */
+                chrome.storage.sync.get('profaneMsgStatus', function (data) {
 
-				// Assign random message (more of just in case they find a way to bypass extension and don't close tab)
-				/** RANDOM MESSAGE GENERATOR */
-				chrome.storage.sync.get('profaneMsgStatus', function(data) {
+                    var profaneMsgStatus = data.profaneMsgStatus;       // Get a local var containing msg status bool
 
-					var profaneMsgStatus = data.profaneMsgStatus;       // Get a local var containing msg status bool
+                    var index = 0;          // Default message index
+                    var notifMsgs;          // A pointer for our local msg array
 
-					var index = 0;      // Default message index
-					var notifMsgs;      // A pointer for our local msg array
+                    // Get a random message
+                    if (profaneMsgStatus == true) {
+                        index = getRandomInt(0, profaneNotifications.length);
+                        notifMsgs = profaneNotifications;
 
-					// Get a random message
-					if (profaneMsgStatus == true) {
-						index = getRandomInt(0, profaneNotifications.length);
-						notifMsgs = profaneNotifications;
+                    } else {
+                        index = getRandomInt(0, cleanNotifications.length);
+                        notifMsgs = cleanNotifications;
 
-					} else {
-						index = getRandomInt(0, cleanNotifications.length);
-						notifMsgs = cleanNotifications;
+                    }
 
-					}
+                    // Generate new index if we get the same index as last time
+                    while (index == lastMsgIndex) {
+                        index = getRandomInt(0, profaneNotifications.length);
+                    }
 
-					// Generate new index if we get the same index as last time
-					while (index == lastMsgIndex) {
-						index = getRandomInt(0, profaneNotifications.length);
-					}
+                    // Set the message text
+                    messageText.innerHTML = notifMsgs[index];
 
-					// Set the message text
-					messageText.innerHTML = notifMsgs[index];
+                    // Prevent same message appearing consecutively
+                    lastMsgIndex = index;
 
-					// Prevent same message appearing consecutively
-					lastMsgIndex = index;
-
-					// Set messageText properties
-					messageText.style.fontSize = "80px";
-					messageText.style.fontStyle = "italic";
-					messageText.style.fontWeight = "900";
-					messageText.style.position = "fixed";
-					messageText.style.color = "black";
-					messageText.style.top = "180px";        // Distance from top
+                    // Set messageText properties
+                    messageText.style.fontSize = "80px";
+                    messageText.style.fontStyle = "italic";
+                    messageText.style.fontWeight = "900";
+                    messageText.style.position = "fixed";
+                    messageText.style.color = "black";
+                    messageText.style.top = "180px";        // Distance from top
                     messageText.style.alignSelf = "parent";
+                    messageText.style.textOverflow = "ellipsis";
 
-				});
+                });
 
-				console.log("LAST INDEX OUTSIDE CALL: " + lastMsgIndex);
+                console.log("LAST INDEX OUTSIDE CALL: " + lastMsgIndex);
 
 
-				/** ORGANIZING MESSAGE + BUTTON HIERARCHY */
+                /** ORGANIZING MESSAGE + BUTTON HIERARCHY */
 
-				// Create break element
-				var breakElement = document.createElement("br");
-                breakElement.setAttribute("style",
-                    "alignSelf: parent");
+                    // Create break element
+                var breakElement = document.createElement("br");
+                breakElement.style.alignSelf = "center";
 
                 // Add messageText to text spawn
-				messageTextSpan.appendChild(messageText);
+                messageTextSpan.appendChild(messageText);
 
                 // Add text spawn to a div
-				messageDiv.appendChild(messageTextSpan);
+                messageDiv.appendChild(messageTextSpan);
 
                 // Insert break elements between message and button
-				messageDiv.appendChild(breakElement);
-				messageDiv.appendChild(breakElement);
+                messageDiv.appendChild(breakElement);
+                messageDiv.appendChild(breakElement);
 
                 // Insert div into greater container
-				messageDivContainer.appendChild(messageDiv);
+                messageDivContainer.appendChild(messageDiv);
 
                 // Insert container into parent div
-				messageParentDiv.appendChild(messageDivContainer);
+                messageParentDiv.appendChild(messageDivContainer);
 
-				// Create button div
-				var buttonDiv = document.createElement("div");
-				buttonDiv.setAttribute("style",
+                // Create button div
+                var buttonDiv = document.createElement("div");
+                buttonDiv.setAttribute("style",
                     "margins: auto; " +
                     "text-align: center; " +
                     "z-index: 2100000000");
 
-				// Create button
-				var button = document.createElement("button");
-				var textNode = document.createTextNode("True that.");
-				button.appendChild(textNode);
+                // Create button
+                var button = document.createElement("button");
+
+                /** Get a random button response */
+
+                // Generate random button response
+                var buttonText = buttonResponses[getRandomInt(0, buttonResponses.length)];
+
+                // Create button with that button response
+                var textNode = document.createTextNode(buttonText);
+                button.appendChild(textNode);
 
                 // Adjust button properties
-				button.setAttribute("style",
+                button.setAttribute("style",
                     "position: fixed; " +
                     "bottom: 15%; " +
                     "z-index: 20000; " +
                     "text-align: center;" +
-					"width: 300px; " +
+                    "width: 300px; " +
                     "height: 150px; " +
                     "color: white; " +
                     "background-color: black; " +
                     "margin: auto; " +
                     "border: 3px solid white;" +
-					"left: 38%; " +
+                    "left: 38%; " +
                     "font-size: 45px; " +
                     "box-shadow: 5px 5px grey; " +
                     "z-index: 2100000001;");  // Background color was #ff0000
 
-				button.style.fontWeight = "lighter";
-				button.style.fontStyle = "italic";
+                button.style.fontWeight = "lighter";
+                button.style.fontStyle = "italic";
 
 
                 // Add button to button div
-				buttonDiv.appendChild(button);
+                buttonDiv.appendChild(button);
 
                 // Add all divs to page
-				document.body.appendChild(backgroundDiv);		// Background layer
-				document.body.appendChild(messageParentDiv);    // Message layer
-				document.body.appendChild(buttonDiv);           // Button layer
-
+                document.body.appendChild(backgroundDiv);		// Background layer
+                document.body.appendChild(messageParentDiv);    // Message layer
+                document.body.appendChild(buttonDiv);           // Button layer
 
 
                 /** BUTTON INTERACTION METHODS */
 
-				button.onmouseover = function() {mouseOver()};
-				button.onmouseout = function() {mouseOut()};
+                button.onmouseover = function () {
+                    mouseOver()
+                };
+                button.onmouseout = function () {
+                    mouseOut()
+                };
 
-				// Change button colors on mouseOver
-				function mouseOver() {
-					button.style.backgroundColor = "white";  // Was #E80C7A
-					button.style.color = "black";
-					button.style.fontWeight = "bolder";
-					button.style.fontStyle = "italic";
-				}
+                // Change button colors on mouseOver
+                function mouseOver() {
+                    button.style.backgroundColor = "white";  // Was #E80C7A
+                    button.style.color = "black";
+                    button.style.fontWeight = "bolder";
+                    button.style.fontStyle = "italic";
+                }
 
-				// Change button colors back on mouseOut
-				function mouseOut() {
-					button.style.backgroundColor = "black";     // Was #ff0000
-					button.style.color = "white";
-					button.style.fontWeight = "lighter";
-					button.style.fontStyle = "italic";
-				}
+                // Change button colors back on mouseOut
+                function mouseOut() {
+                    button.style.backgroundColor = "black";     // Was #ff0000
+                    button.style.color = "white";
+                    button.style.fontWeight = "lighter";
+                    button.style.fontStyle = "italic";
+                }
 
-				// Send tab closure message to background script on button click
-				window.onclick = function (event) {
-					if (event.target == button) {
-						chrome.runtime.sendMessage("closeTab");		// Send message to background script
-					}
-				}
-			}
+                // Send tab closure message to background script on button click
+                window.onclick = function (event) {
+                    if (event.target == button) {
+                        chrome.runtime.sendMessage("closeTab");		// Send message to background script
+                    }
+                }
+            }
 		}
 	});
 }
 
+/** HELPER METHODS */
 
 // RNG for string message array
 function getRandomInt(min, max) {
@@ -310,18 +306,49 @@ function getRandomInt(min, max) {
 }
 
 
+/** POTENTIAL MESSAGES */
+
+profaneNotifications =  [
+    "You've been watching too many videos! Your laziness ain't helping nobody. Get your shit together.",
+    "... really? Fuck your bullshit. Get to work, asshat.",
+    "You're lazy. How does it feel? Is this what you want to be forever? Can't say I expected much better.",
+    "Ain't nobody gonna hold your hand in this tough world. Start busting ass and taking names now.",
+    "Life ain't all sunshines and YouTube. Take the pledge to better yourself.",
+    "Thought is nothing. Execution is everything. Close this fucking tab.",
+    "Get moving. Whether that's in the mind or body, just get moving."
+];
+
+cleanNotifications =    [
+    "You've been watching too many videos! Your laziness ain't helping nobody. Get it together.",
+    "This ain't the way to go. Get off your bum.",
+    "You're lazy. How does it feel? Is this what you want to be forever? Can't say I expected much better.",
+    "Ain't nobody gonna hold your hand in this tough world. Start working hard now before it's too late.",
+    "Life ain't all sunshines and YouTube. Take the pledge to better yourself.",
+    "Thought is nothing. Execution is everything. Close the tab.",
+    "Get moving. Whether that's in the mind or body, just get moving."
+];
+
+buttonResponses =       [
+    "True that.",
+    "Got it.",
+    "Aw, snap...",
+    "Damn."
+];
+
+
+
 
 /**************************************   MAIN   **************************************/
 
-// YouTube detection logic
+// Logic that detects entering a new YouTube video watch page
 (document.body || document.documentElement).addEventListener('transitionend',
-	function (/*TransitionEvent*/ event) {
+	function (event) {
 		if (event.propertyName === 'width' && event.target.id === 'progress') {
-			afterNavigate();
+			navigatedToNewPage();
 		}
 	}, true);
 
-// After page load
-afterNavigate();
+// After the new page is loaded
+navigatedToNewPage();
 
 
