a9a49c320e7935ae1f7e78d144192b870f69e5d9
Michael Hsiu <michael.hsiu@berkeley.edu>
2017-02-26 00:30:06 -0800

    Slight callback fixes to content -callbackFix.js. ToDo: move logic for total video count using local storage from v2 version of plugin to this content -callbackFix.js. See if there's anything else that's new that's missing. Organize code and try to separate UI from logic.

--- YouTube Blocker v1/background.js
+++ YouTube Blocker v1/background.js
@@ -1,135 +1,60 @@
-// Receives message from content script to close tab when user clicks on button
+/** MAIN */
+
+// Holds info about closing all tabs
+var closeAllGlobal;
+
+// Set listener for button click
 chrome.runtime.onMessage.addListener(function(response, sender, sendResponse) {
-	// response holds content.js message
-	// sender holds info about info about tab sending info to this script
-	// sendResponse sends response back to content.js, if needed
 
-	var closeAllGlobal;
+	// Should we close all tabs? Chooses and executes correct tab closure function.
+	chrome.storage.sync.get('closeAll', chooseTabFunction);
 
-	// Should we close all tabs?
-	chrome.storage.sync.get('closeAll', function(data) {
-			tabData = data.closeAll;
-			closeAllGlobal = tabData;
-
-		//alert("Close all tabs?: " + closeAllGlobal);
-		// Either close all YouTube tabs...
-		if (closeAllGlobal == true) {
-			//alert("INSIDE IF");
-
-			chrome.tabs.query({}, function(tabs) {		
-
-				var substring = "youtube.com";
-
-	    		// Close all YouTube tabs
-	    		for (i = 0; i < tabs.length; i++) {
-	    			if (tabs[i].url.includes(substring)) {
-						chrome.tabs.remove(tabs[i].id);
-	    			}
-	    		}
-			});
-
-		// ... or only close the current YouTube tab
-		} else {
-			// Get the current active tab
-			//alert("INSIDE ELSE");
-			chrome.tabs.query({ currentWindow: true, active: true }, function(tabs) {		
-				var current = tabs[0];
-				// Close current active tab
-				chrome.tabs.remove(current.id);
-			});
-		}
-	});
 });
 
-	
 
+/** METHODS */
 
+function chooseTabFunction (data) {
 
+	// Set tab closure status
+	closeAllGlobal = data.closeAll;
 
-/** WORKING, closes active tab. 
-	if (response === "closeTab") {
-		// Get the current active tab
-		chrome.tabs.query({ currentWindow: true, active: true }, function(tabs) {		
-			var current = tabs[0];
-			// Close current active tab
-			chrome.tabs.remove(current.id);
-		});
+	// Either close all YouTube tabs...
+	alert(closeAllGlobal);
+	if (closeAllGlobal == true) {
+
+		// Close all tabs
+		chrome.tabs.query({}, closeAllTabs);
+
+	// ... or only close the current YouTube tab
+	} else {
+
+		// Close current tab
+		chrome.tabs.query({ currentWindow: true, active: true }, closeCurrentTab);
 	}
-}); */
-    	/*chrome.tabs.query({}, function(tabs) {
-			
-    		var closeAll;
-    		// Should we close all tabs?
-			chrome.storage.sync.get('closeAll', function(data) {
-				closeAll = data.closeAll;
-				}
-			});
-
-			if (closeAll) {
-				var substring = "youtube.com";
-
-	    		// Close all YouTube tabs
-	    		for (i = 0; i < tabs.length; i++) {
-	    			if (tabs[i].url.includes(substring)) {
-						chrome.tabs.remove(tabs[i].id);
-	    			}
-	    		}
-			} else {
-				var current = tabs[0];
-				// Close current active tab
-				chrome.tabs.remove(current.id);
-			}
-		});*/
+}
 
 
-		/*chrome.tabs.query({},function(tabs) {     
-    		console.log("\n/////////////////////\n");
-    		tabs.forEach(function(tab){
-      			console.log(tab.url);
-    		});
- 		});*/
+function closeAllTabs (tabs) {
 
-		/*// Closes all YouTube tabs
-		chrome.tabs.query({}, function (tabs) {
-    		for (var i = 0; i < tabs.length; i++) {
-       			if (tabs[i].url.indexOf(substring) !== -1) {
-					chrome.tabs.remove(tabs[i].id);
-    			}
-    		}
-		});*/
+	var substring = "youtube.com";
 
-		/*// Closes all tabs [WORKS]
-		chrome.tabs.query({}, function (tabs) {
-    		for (var i = 0; i < tabs.length; i++) {
-       		 chrome.tabs.remove(tabs[i].id);
-    		}
-		}); */
+	// Close all YouTube tabs
+	for (i = 0; i < tabs.length; i++) {
+		if (tabs[i].url.includes(substring)) {
+			chrome.tabs.remove(tabs[i].id);
+		}
+	}
+}
+
+
+function closeCurrentTab (tabs) {
+
+	var current = tabs[0];
+
+	// Close current active tab
+	chrome.tabs.remove(current.id);
+}
 
 
 
-
-
-/*// Called when the user clicks on the browser action.
-chrome.browserAction.onClicked.addListener(function(tab) {
-	// No tabs or host permissions needed!
-	console.log('Turning ' + tab.url + ' red!');
-	chrome.tabs.executeScript({
-		code: 'document.body.style.backgroundColor="red"'
-	}); 
-
-
-
-});*/
-
-/*
-	var activeTabId;
-
-	chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-		activeTabId = tabs[0];
-	});
-	chrome.tabs.remove(activeTabId);
-
-  	chrome.tabs.getSelected(null, function(tab) {
-    	chrome.tabs.remove(tab.id);
-  	});
-*/
